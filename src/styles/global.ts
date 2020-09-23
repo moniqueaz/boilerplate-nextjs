2dbed0939b765ae86b52d61986500990511ca332
Monique Azevedo da Silva <monique.azevedo@BRRJOBITAIM2724.local>
2020-09-23 08:49:11 -0300

    Ajusta modo import storybook e corrige global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,23 +1,47 @@
 import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 300;
+    font-display: swap;
+    src: local('Poppins Light'), local('Poppins-Light'), url('/fonts/poppins-v13-latin-300.woff2') format('woff2')
+  }
+
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: local('Poppins Regular'), local('Poppins-Regular'), url('/fonts/poppins-v13-latin-regular.woff2') format('woff2')
+  }
+
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    font-display: swap;
+    src: local('Poppins SemiBold'), local('Poppins-SemiBold'), url('/fonts/poppins-v13-latin-600.woff2') format('woff2')
+  }
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   ${({ theme }) => css`
     html {
       font-size: 62.5%;
     }
-    html,
-    body,
-    #__next {
+    body {
       font-family: ${theme.font.family};
       font-size: ${theme.font.sizes.medium};
     }
   `}
+
 `
 
 export default GlobalStyles
