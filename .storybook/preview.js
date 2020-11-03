84ff3735a61272fd82677ebfb63cbdbe6ae3aa91
Monique Azevedo da Silva <monique.azevedo@BRRJOBITAIM2724.local>
2020-11-02 21:20:40 -0300

    adiciona background do theme no storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,10 +2,26 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from "styles/global";
 import theme from 'styles/theme'
 
+export const parameters = {
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: theme.colors.white
+      },
+      {
+        name: 'dark',
+        value: theme.colors.mainBg
+      }
+    ]
+  }
+}
+
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
-      <GlobalStyles/>
+      <GlobalStyles removeBg/>
       <Story />
     </ThemeProvider>
   ),
